4a0d63cf584de812b3719f13cfe08c6902d79a23
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2022-04-07 19:44:12 -0500

    main fixes12

--- src/modules/subnet/subnet.tf
+++ src/modules/subnet/subnet.tf
@@ -5,7 +5,7 @@
 resource "google_compute_subnetwork" "subnet" {
 	project = var.project_id	
 	name = var.subnet_name
-    ip_cidr_range = var.cidr_range
+    ip_cidr_range = var.subnet_cidr_range
     network = var.network_name	 # google_compute_network.vpc-network.self_link
     region = var.region
     private_ip_google_access = var.private_ip_google_access
