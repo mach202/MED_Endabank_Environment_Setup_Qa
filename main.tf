9e3b8320c4a4a9787123f792d513617c97b4d506
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2022-04-21 18:35:05 -0500

    change database to mysql

--- main.tf
+++ main.tf
@@ -186,7 +186,7 @@ module "database" {   #database module
 
     database_instance_name = "med-endabank-database-primary1"
     database_region = var.region
-    database_version = "POSTGRES_13"
+    database_version = "MYSQL_5_7"#"POSTGRES_13"
     deletion_protection = false
     depends_on_database = [module.database.depends-on-database]#[google_service_networking_connection.private_vpc_connection]
     database_tier = "db-g1-small"
