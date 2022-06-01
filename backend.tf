e9e73abab7f110a63f89a784a819269826f00aa5
unknown <santi78es@gmail.com>
2022-06-01 11:59:28 -0500

    final final este si

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "remote"{
       hostname ="app.terraform.io"
-      organization = "Medellin-Med-Endabank-qa"
+      organization = "Medellin-Med-Endabank-qa-1"
 
     workspaces {
       name = "MED_Endabank_Environment_Setup_Qa"
